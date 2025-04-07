9f538e491814d19fca3e2e01ec9a1d526add6ad5
ed-RG <ed.carroll@red-gate.com>
2025-04-07 09:46:22 +0100

    Add Example.cs

---
+++ code/Example.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, world!");
+    }
+
+    static int AddNumbers(int a, int b)
+    {
+        return a + b;
+    }
+
+    static int AddMaxAndMinOfTwoEnumerables(IEnumerable<int> maxEnumerable, IEnumerable<int> minEnumerable)
+    {
+        return maxEnumerable.Min() + minEnumerable.Min();
+    }
+}
